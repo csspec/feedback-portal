139cb4c99b8a6262de69e3339fb33d29d0b22d98
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-10-29 11:06:36 +0530

    [Controllers] Added implementation of checValidity

--- src/main/java/org/csspec/feedback/controllers/CheckTokenController.java
+++ src/main/java/org/csspec/feedback/controllers/CheckTokenController.java
@@ -2,12 +2,16 @@ package org.csspec.feedback.controllers;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.csspec.feedback.config.Jwt;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.csspec.feedback.helpers.Account;
+import org.csspec.feedback.helpers.HeaderRequestInterceptor;
+import org.omg.PortableInterceptor.ClientRequestInterceptor;
+import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.Cookie;
 import java.text.DateFormat;
@@ -20,6 +24,9 @@ public class CheckTokenController {
 
     private static final String COOKIE_NAME = "feedback_csspec_org";
     private static final String SESSION_COOKIE_NAME = "CSS_FEEDBACK_SESSION_USER_ID";
+    // store the secret as an environment variable
+    private static final String SECRET = "secret";
+    private static final String CLIENT = "feedback";
 
     public static boolean checkCookie(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
@@ -42,25 +49,42 @@ public class CheckTokenController {
             System.out.println(e);
             return false;
         }
-        if (checkValidity(hash) != null) {
-            return true;
-        }
-        return false;
+        return checkValidity(hash) != null;
     }
 
+
     public static Map<String, String> checkValidity(String hash) {
-        // TODO: make a request to auth server to check the validity of the token
-        try {
-            Thread.sleep(200);
-        } catch (Exception e) {
-            System.out.println(e);
+        final String uri = "http://localhost:8090/oauth/check_token";
+        System.out.println("Checking token: " + hash);
+
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new HeaderRequestInterceptor("Authorization", "Bearer " + hash));
+        interceptors.add(new HeaderRequestInterceptor("x-auth-token", "client_id=" + CLIENT + "&client_secret=" + SECRET));
+        interceptors.add(new HeaderRequestInterceptor("Content-Type", "application/json"));
+        RestTemplate template = new RestTemplate();
+        template.setInterceptors(interceptors);
+        Account account = template.postForObject(uri, null, Account.class);
+        return Collections.singletonMap("user_id", account.getUserid());
+    }
+
+    public static class Token {
+        private String hash;
+
+        public Token() {}
+
+        public void setHash(String hash) {
+            this.hash = hash;
         }
-        return Collections.singletonMap("user_id", "2");
+
+        public String getHash() {
+            return hash;
+        }
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> checkToken(Map<String, String> request) {
-        String hash = request.get("hash");
+    public ResponseEntity<?> checkToken(@RequestBody Token request) {
+        String hash = request.getHash();
+        System.out.println("Handling check_token: hash: " + hash);
         String redirect_uri = "/error?error_type=invalid_token&token=" + hash;
         Map<String, String> map = checkValidity(hash);
         if (map != null) {
